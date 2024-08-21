8cb2b86207ee381414722766ed5f097d28dde957
Moros Smith <moros1138@gmail.com>
2024-08-21 12:33:09 -0400

    app.js boilerplate

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { v4 as uuid4 } from 'uuid';
+import { Filter } from 'bad-words';
+
+const filter = new Filter();
+
+export default function defineApi(app)
+{
+}
+
+
+
