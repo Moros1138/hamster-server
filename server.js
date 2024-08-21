f4c834686044667eb8e0974d0d5a312af4841524
Moros Smith <moros1138@gmail.com>
2024-08-21 16:35:59 -0400

    server boilerplate

---
+++ server.js
@@ -0,0 +1,57 @@
+import 'dotenv/config';
+
+import defineApi from "./app.js";
+
+import express from 'express';
+
+const port            = process.env.PORT || "8000";
+const publicDirectory = process.env.PUBLIC_DIRECTORY || "public";
+const sessionName     = process.env.SESSION_NAME   || "sessionid";
+const sessionSecret   = process.env.SESSION_SECRET || "$eCuRiTy";
+
+import Database from 'better-sqlite3';
+import session from 'express-session';
+import store from 'better-sqlite3-session-store';
+
+const app = express();
+
+const SqliteStore = store(session);
+
+const races = new Database("races.db", { verbose: console.log });
+const sessions = new Database("sessions.db", { verbose: console.log });
+
+races.exec(`CREATE TABLE IF NOT EXISTS 'races' (
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+    'color' TEXT,
+    'name' TEXT,
+    'map' TEXT,
+    'time' INTEGER
+)`);
+
+app.use(express.static(publicDirectory));
+
+app.use(express.json());
+
+app.use(session({
+    name: sessionName,
+    store: new SqliteStore({
+        client: sessions, 
+        expired: {
+            clear: true,
+            intervalMs: 900000 //ms = 15 minutes
+        }
+    }),
+    saveUninitialized: false,
+    secret: sessionSecret,
+    resave: false,
+    cookie: {
+        sameSite: "strict"
+    }
+}));
+
+defineApi(app, races);
+
+app.listen(port, () =>
+{
+    console.log(`Listening on http://localhost:${port}`);
+});
