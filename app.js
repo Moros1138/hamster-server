9a406efb78957a2f00470bce9c76ee8d97d3721a
Moros Smith <moros1138@gmail.com>
2024-08-21 23:04:23 -0400

    switch to obscenity for bad word filtering

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { v4 as uuid4 } from 'uuid';
-import { Filter } from 'bad-words';
 
-const filter = new Filter();
+import {
+	RegExpMatcher,
+	TextCensor,
+	englishDataset,
+	englishRecommendedTransformers,
+} from 'obscenity';
 
-
-
+const matcher = new RegExpMatcher({
+	...englishDataset.build(),
+	...englishRecommendedTransformers,
+});
 
 function ExtractMissingParameters(requiredParams, request)
 {
